ca3e2d3f9bc6081cf3e15da692152b30d8ef46ba
Serena Wong <serenaw@stanford.edu>
2015-07-22 22:55:58 +0200

    working on new; post + /articles/new route errer

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,24 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    @place.save
+    redirect_to @place
+  end
+
+  private
+    def place_params
+      params.require(:place).permit(:name, :description, :rating, :must_see)
+    end
+
 end
