62ef97b00b87d5c73cd2ecbac0ab42c96cad5943
Serena Wong <serenaw@stanford.edu>
2015-07-23 11:43:58 +0200

    finished basic week 2 project

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,15 +11,36 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   def create
     @place = Place.new(place_params)
     @place.save
     redirect_to @place
   end
 
+  def update #not updating waterfront location
+    @place = Place.find(params[:id])
+
+    if @place.update(place_params)
+      redirect_to @place
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+
+    redirect_to places_path
+  end
+
   private
     def place_params
-      params.require(:place).permit(:name, :description, :rating, :must_see)
+      params.require(:place).permit(:name, :description, :location, :rating, :must_see)
     end
 
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,3 @@
 class Place < ActiveRecord::Base
+  validates :name, :description, :location, :rating, presence: true
 end
